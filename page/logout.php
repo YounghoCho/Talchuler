51e09df1a99b29a7652a99d9010ff30439a97c0b
YounghoCho <dodghek1@naver.com>
2017-01-10 01:06:38 +0900

    Add files via upload

---
+++ page/logout.php
@@ -0,0 +1,15 @@
+<?
+session_start();
+?>
+
+<?php
+include ("./include.php");
+
+$_SESSION["user_email"] = "";  
+$_SESSION["user_nickname"] = ""; 
+2016-08-18
+?>
+
+<script>
+location.replace("../index.php");
+</script>
