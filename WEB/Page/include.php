50448a7c606d57da35082feeee8af83175d041ac
Young Ho <dodghek1@naver.com>
2017-01-21 01:35:39 +0900

    remove Origin Page directory

--- WEB/Page/include.php
+++
@@ -1,9 +0,0 @@
-<?
-$db_host = "localhost";
-$db_user = "";
-$db_pass = "";
-
-$result = mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
-mysql_select_db("") or die(mysql_error());
-mysql_query("SET CHARACTER SET utf8");
-?>
